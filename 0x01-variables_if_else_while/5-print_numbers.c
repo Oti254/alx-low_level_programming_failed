70b610a226558cac83a724eb6f9dc85f14126c00
Oti254 <otrated555@gmail.com>
2023-03-23 20:18:22 +0000

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - program that prints single digits from 0 to 10
+ *
+ * Return: 0 (Success)
+ *
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+	printf("%d", i);
+}
+return (0);
+}
