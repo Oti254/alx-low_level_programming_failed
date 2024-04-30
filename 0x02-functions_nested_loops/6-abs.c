7f8c8a863b476285e3df2e7d8227bc089a1fe1fe
Oti254 <git@github.com:Oti254/alx-low_level_programming.git>
2024-04-30 20:53:02 +0000

    There is no such thing as absolute value in this world. You can only estimate what a thing is worth to you

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value
+ *
+ * @n: integer checked
+ *
+ * Return: Always 0
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = -n;
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
+	return (0);
+}
