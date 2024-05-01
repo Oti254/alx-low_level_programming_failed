0cf047bfc80fcf0a1f5f3ccaf6a65b45355509a8
Oti254 <git@github.com:Oti254/alx-low_level_programming.git>
2024-05-01 13:40:49 +0000

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *
+ * @a: integer to be added
+ *
+ * @b: integer to be added
+ *
+ * Return: returns the result
+ *
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: ...
+ *
+ */
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h <= 24; h++)
+	{
+		for (m = 00; m <= 59; m++)
+		{
+			return (m);
+		}
+		return (h);
+	}
+}
