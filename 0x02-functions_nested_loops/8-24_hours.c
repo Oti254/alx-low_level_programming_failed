eadf818e5318ed121d3fdf4bde3e6f58c1b35d96
Oti254 <git@github.com:Oti254/alx-low_level_programming.git>
2024-05-06 17:18:21 +0000

    8-24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,22 +8,22 @@
  */
 void jack_bauer(void)
 {
-	int a, b, c, d;
+	int h, b, m, d;
 
-	for (a = 0; a <= 2; a++)
+	for (h = 0; h <= 2; h++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
+			if ((h <= 1 && b <= 9) || (h <= 2 && b <= 3))
 			{
-				for (c = 0; c <= 5; c++)
+				for (m = 0; m <= 5; m++)
 				{
 					for (d = 0; d <= 9; d++)
 					{
-						_putchar(a + '0');
+						_putchar(h + '0');
 						_putchar(b + '0');
 						_putchar(58);
-						_putchar(c + '0');
+						_putchar(m + '0');
 						_putchar(d + '0');
 						_putchar('\n');
 					}
