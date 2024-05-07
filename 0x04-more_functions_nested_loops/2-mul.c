c73c8258634292ccdbc014ddba23bde9975b20f2
Oti254 <git@github.com:Oti254/alx-low_level_programming.git>
2024-05-07 23:17:35 +0000

    Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ *
+ * @a: First integer to be multiplied
+ *
+ * @b: Second integer to be multiplied
+ *
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
