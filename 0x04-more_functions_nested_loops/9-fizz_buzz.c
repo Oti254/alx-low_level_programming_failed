a687b5ecab31fa66664ec04bf2c56899b08b48f1
Oti254 <git@github.com:Oti254/alx-low_level_programming.git>
2024-05-08 13:41:37 +0000

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			printf("Fizz Buzz ");
+			printf("FizzBuzz ");
 		}
 		else if (i % 3 == 0)
 		{
