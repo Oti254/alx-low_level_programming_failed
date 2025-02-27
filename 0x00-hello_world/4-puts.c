7b8f4f512b17c9a075d7829df70b4c4ba4dd6cad
Oti254 <otrated555@gmail.com>
2025-02-28 01:16:43 +0300

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string and new line
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
